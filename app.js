15eb9ad8886b56ffc31e30f46dbe794b5a36780c
elmehdibilla <billamehdi13@gmail.com>
2026-06-13 15:42:36 +0100

    solution for exercise 11.12 (fix)

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  '
   res.send('ok')
 })
 
